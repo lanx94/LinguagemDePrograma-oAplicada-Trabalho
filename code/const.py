5ad503305dea745dd22cfc61336758d81e7014b7
Lucas <lucascardoso94@outlook.com>
2026-06-28 18:18:20 -0300

    Fim da segunda aula prática

---
+++ code/const.py
@@ -0,0 +1,11 @@
+# C
+COLOR_WHITE = (255, 255, 255)
+
+# M
+MENU_OPTION = ('New Game Single',
+               'New Game Coop - Unavailable',
+               'Score',
+               'Exit')
+
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import pygame
+from code.game import Game
 
-
-pygame.init()
-window = pygame.display.set_mode((800, 600))
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            print("Quitting")
-            pygame.quit()
-            quit()
+game = Game()  # Cria um objeto instanciando a classe game
+game.run()  # roda o metodo da classe Game
