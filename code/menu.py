8a1c5563dd0b9d3f9a0406223a4f1df476042014
Lucas <lucascardoso94@outlook.com>
2026-06-28 18:18:27 -0300

    Fim da segunda aula prática

---
+++ code/menu.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import pygame.image
+from pygame.font import Font
+from pygame import Surface, Rect
+
+from code.const import WIN_WIDTH, MENU_OPTION, COLOR_WHITE
+
+
+class Menu:
+    def __init__(self, window):
+        self.window = window
+        self.surf = pygame.image.load('./asset/background/city 1/menuBackground.png')
+        self.rect = self.surf.get_rect(left=0, top=0)
+
+    def run(self, window):
+        pygame.mixer.music.load('asset/music/music1.mp3')
+        pygame.mixer.music.play(-1)
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(50, "City Under", COLOR_WHITE, ((WIN_WIDTH / 2), (70)))
+            self.menu_text(100, "Attack", COLOR_WHITE, ((WIN_WIDTH / 2), (120)))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(25, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), (200 + 25 * i)))
+
+            pygame.display.flip()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
